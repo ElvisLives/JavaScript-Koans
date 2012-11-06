98b39cd87c6bb7969223b2bf615fb399c862178b
ElvisLives <code@ps.otherinbox.com>
2012-11-06 09:05:44 -0700

    about_assignment, control, equality, truthyness changes

--- topics/about_truthyness.js
+++ topics/about_truthyness.js
@@ -1,22 +1,36 @@
-
+// http://www.sitepoint.com/javascript-truthy-falsy/
 module("About Truthyness (topics/about_truthyness.js)");
 
 test("truthyness of positive numbers", function() {
     var oneIsTruthy = 1 ? true : false;
-    equals(oneIsTruthy, __, 'is one truthy?');
+    equals(oneIsTruthy, true, 'is one truthy?');
 });
 
 test("truthyness of negative numbers", function() {
     var negativeOneIsTruthy = -1 ? true : false;
-    equals(negativeOneIsTruthy, __, 'is -1 truthy?');
+    equals(negativeOneIsTruthy, true, 'is -1 truthy?');
 });
 
 test("truthyness of zero", function() {
     var zeroIsTruthy = 0 ? true : false;
-    equals(zeroIsTruthy, __, 'is 0 truthy?');
+    equals(zeroIsTruthy, false, 'is 0 truthy?');
 });
 
 test("truthyness of null", function() {
     var nullIsTruthy = null ? true : false;
-    equals(nullIsTruthy, __, 'is null truthy?');
+    equals(nullIsTruthy, false, 'is null truthy?');
 });
+
+test("truthyness of null against undefined", function () {
+    ok(null == undefined, 'is null truthy against undefined?');
+});
+
+test("truthyness of undefined against undefined", function () {
+    ok(undefined == undefined, 'is undefined truthy against undefined?');
+});
+
+test("truthyness of NaN against undefined", function () {
+    ok(NaN != undefined, 'is NaN ever truthy');
+});
+
+
